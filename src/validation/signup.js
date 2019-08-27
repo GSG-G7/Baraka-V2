f63dc1aba19e6f01d812bfa9df7f4303709afa91
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-27 16:07:00 +0300

    server side validation using @hapi/joi package relates #9

---
+++ src/validation/signup.js
@@ -0,0 +1,16 @@
+const Joi = require('@hapi/joi');
+
+const signupSchema = Joi.object().keys({
+  username: Joi.string()
+    .alphanum()
+    .min(2)
+    .max(30)
+    .required(),
+  password: Joi.string()
+    .regex(/^[a-zA-Z0-9]{8,30}$/)
+    .required(),
+  confirmPassword: Joi.ref('password'),
+  email: Joi.string().email({ minDomainSegments: 2 })
+});
+
+module.exports = loginData => Joi.validate(loginData, signupSchema);
