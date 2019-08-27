592f80f3ca6b0b9788a71b8045017eb9b725ef97
fadi omar <fadeomar2015@gmail.com>
2019-08-27 12:05:39 +0300

    create database schema Relates #1

---
+++ src/models/config/db_build.sql
@@ -0,0 +1,20 @@
+BEGIN;
+DROP TABLE IF EXISTS baraka_item, baraka_list, baraka_user; 
+CREATE TABLE baraka_list (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+CREATE TABLE baraka_user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR NOT NULL,
+    email VARCHAR NOT NUll
+);
+CREATE TABLE baraka_item (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30),
+    content VARCHAR,
+    list_id INTEGER REFERENCES baraka_list(id),
+    user_id INTEGER REFERENCES baraka_user(id)
+);
+COMMIT;
