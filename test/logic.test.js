31db8611db92957276cb3162873944631757f224
fadi omar <fadeomar2015@gmail.com>
2019-08-27 12:09:01 +0300

    enabled a test for travis to pass

--- test/logic.test.js
+++ test/logic.test.js
@@ -1,6 +1,6 @@
-// const tape = require('tape');
+const tape = require('tape');
 
-// tape('init logic test', t => {
-//   t.equal(1, 1, 'should pass');
-//   t.end();
-// });
+tape('init logic test', t => {
+  t.equal(1, 1, 'should pass');
+  t.end();
+});
