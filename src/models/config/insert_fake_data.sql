97c039509916fac38853450ce28b2b6c4b9c8d24
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-27 12:55:17 +0300

    insert data to db using insert_fake_data.sql file relates #2

---
+++ src/models/config/insert_fake_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO baraka_user
+    (username , password , email)
+VALUES
+    ('Mohammed-Q96-1' , 'password1' , 'codecademy'),
+    ('Mohammed-Q96-2' , 'password2' , 'codecademy'),
+    ('Mohammed-Q96-3' , 'password3' , 'codecademy');
+
+INSERT INTO baraka_item
+    (list_id , user_id ,content, isDone)
+VALUES
+    ( 1 , 1, 'some dumy text', true),
+    ( 2 , 2, 'some dumy text', true),
+    ( 3 , 3, 'some dumy text', false);
+
+INSERT INTO baraka_list
+    (name)
+VALUES
+    ('some dumy text'),
+    ('some dumy text2'),
+    ('some dumy text3');
